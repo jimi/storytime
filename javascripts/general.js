750cbc4c3137e735eee6a3e6f02e3edd0a8778a0
jimi <jim@arizonabay.com>
2023-12-26 17:24:55 +0800

    Update general.js

--- javascripts/general.js
+++ javascripts/general.js
@@ -1,9 +1,14 @@
+var chapter;
+
+$( document ).ready(function() {
+	chapter = $("body").attr("data-chapter");
+});
+
 $(".homeBtn").on("click", function(event) {  
 	window.location.replace("../index.html");
 });
 
 $(".nextBtn").on("click", function(event) {
-	const chapter = $("body").attr("data-chapter");
 	const index = $(this).attr("data-index");
 	const next = parseInt(index) + 1;
 	
@@ -14,4 +19,22 @@ $(".nextBtn").on("click", function(event) {
     $("#page" + next).fadeIn();	
 		narration.play();
   });
+});
+
+$(".backBtn").on("click", function(event) {
+	const index = $(this).attr("data-index");
+	var back = parseInt(index) - 1;
+	if (back < 0) { back = 0; }
+	
+	$("#page" + index).fadeOut("slow", () => {
+    $("#page" + back).fadeIn();	
+		// todo
+  });
+});
+
+$(".replayBtn").on("click", function(event) {
+	const index = $(this).attr("data-index");
+	const narration = new Audio("../audio/chapter" + chapter + "/page" + index + ".m4a");
+	narration.preload = 'auto';
+	narration.play();
 });
